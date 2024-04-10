3a57baed6f7ba7d9ee42a29023922075a7b17c12
veeralakshmi51 <veeralakshmi@elonnativesystem.com>
2024-04-10 12:04:24 +0530

    Pokemon App

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { Platform, SafeAreaView, StyleSheet,View } from "react-native";
+import {Platform, SafeAreaView, ScrollView, StyleSheet } from "react-native";
 import Card from "./Components/PokemonCard";
 const App =()=>{
   const image1={
@@ -8,19 +8,50 @@ const App =()=>{
     hp:39,
     moves:['Scratch','Ember','Growl','Leer'],
     weaknesses:['Water','Rock'],
+  }; 
+  const image2 = {
+    name: "Squirtle",
+    image: require("./assets/squirtle.png"), 
+    type: "Water",
+    hp: 44,
+    moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
+    weaknesses: ["Electric", "Grass"],
   };
+
+  const image3 = {
+    name: "Bulbasaur",
+    image: require("./assets/bulbasaur.png"), 
+    type: "Grass",
+    hp: 45,
+    moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
+    weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
+  };
+
+  const image4= {
+    name: "Pikachu",
+    image: require("./assets/pikachu.png"), 
+    type: "Electric",
+    hp: 35,
+    moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
+    weaknesses: ["Ground"],
+  };
+
 return(
   <SafeAreaView style={styles.container}>
+  <ScrollView>
     <Card {...image1}/>
+    <Card {...image2}/>
+    <Card {...image3}/>
+    <Card {...image4}/>
+  </ScrollView>
   </SafeAreaView>
   
 )
 }
-
 const styles=StyleSheet.create({
   container:{
     flex:1,
-    backgroundColor:'#f5f5f5',
+    backgroundColor:'#f5f5f5',//white smoke color
     paddingTop:Platform.OS==='android'? '15%':0
   }
 })
--- Components/PokemonCard.js
+++ Components/PokemonCard.js
@@ -1,6 +1,20 @@
 import {View,Text,StyleSheet, Platform,Image} from 'react-native'
- 
+ const typeDetails=(type)=>{
+    switch(type.toLowerCase()){
+        case 'electric':
+            return{borderColor:'#FFD700',emoji:'⚡'};
+        case 'water':
+            return{borderColor:'#6493EA',emoji:'💧'};
+        case 'fire':
+            return{borderColor:'#FF5733',emoji:'🔥'};
+        case 'grass':
+            return{borderColor:'#66CC66',emoji:'☘️'};
+        default:
+            return{borderColor:'#A0A0A0', emoji:'❓'}
+    }
+ };
 const Card=({name,image,type,hp,moves,weaknesses})=>{
+    const {borderColor,emoji}=typeDetails(type);
     return (
 <View style={styles.card}>
     <View style={styles.nameContainer}>
@@ -8,8 +22,11 @@ const Card=({name,image,type,hp,moves,weaknesses})=>{
         <Text style={styles.hp}>❤️{hp}</Text>
     </View>
     <Image source={image} accessibilityLabel={`${name} pokemon`} style={styles.image} resizeMode='contain'/>
-    <View style={styles.typecontainer}>
-        <Text style={styles.type}>{type}</Text>
+    <View style={styles.typeContainer}>
+    <View style={[styles.badge,{borderColor}]}>
+    <Text style={styles.typeEmoji}>{emoji}</Text>
+    <Text style={styles.type}>{type}</Text>
+    </View>
     </View>
     <View style={styles.moveContainer}>
         <Text style={styles.move}>Moves : {moves.join(", ")}</Text>
@@ -22,7 +39,7 @@ const Card=({name,image,type,hp,moves,weaknesses})=>{
 }
 const styles=StyleSheet.create({
    card:{
-     backgroundColor:'lightblue',
+     backgroundColor:'white',
      borderRadius:15,
      borderWidth:2,
      padding:16,
@@ -44,7 +61,7 @@ const styles=StyleSheet.create({
    nameContainer:{
     flexDirection:'row',
     justifyContent:'space-between',
-    marginBottom:30
+    marginBottom:20
    },
    name:{
     fontSize:30,
@@ -56,7 +73,41 @@ const styles=StyleSheet.create({
    image:{
     width:'100%',
     height:200
+   },
+   typeContainer:{
+    marginBottom:30,
+    alignItems:'center',
+    marginTop:10
+   },
+   badge:{
+    flexDirection:'row',
+    alignItems:'center',
+    paddingVertical:2,
+    paddingHorizontal:10,
+    borderRadius:20,
+    borderWidth:4,
+   },
+   typeEmoji:{
+    fontSize:30,
+    marginRight:12,
+   },
+   type:{
+    fontSize:22,
+    fontWeight:'bold'
+   },
+   moveContainer:{
+    marginBottom:16
+   },
+   move:{
+    fontSize:22,
+    fontWeight:'bold'
+   },
+   weakcontainer:{
+    marginBottom:8
+   },
+   weak:{
+    fontSize:22,
+    fontWeight:'bold'
    }
-
 })
 export default Card
